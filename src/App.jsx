fda1855832ab320f7ba305f7e6bcf3038845e00c
KIJ <dlrwn0730@naver.com>
2025-08-11 13:43:45 +0900

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
-import UserList from './components/UserList'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+import Company from './pages/Company'
+import AboutDetail from './pages/AboutDetail'
 
 function App() {
   return (
-    <div>
-      <UserList></UserList>
+    <div className='container'>
+      <h1>라우터 실습</h1>
+      <nav>
+        <Link to='/'>홈</Link> ||
+        <Link to='/about'> 소개</Link> ||
+        <Link to='/company'> 회사</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/about/:id' element={<AboutDetail />} />
+        <Route path='/company' element={<Company />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+
     </div>
   )
 }
