697e90476559fc399613c8cd907f62dd9cc9a62a
KIJ <dlrwn0730@naver.com>
2025-08-12 15:12:50 +0900

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // App.jsx
 import './App.css';
-import Counter from './components/Counter';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div>
-      <Counter />
+      <TodoList />
     </div>
   );
 }
